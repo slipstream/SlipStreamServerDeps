c75400ab7c4cd8f6a970e6132fbaa37feac3f190
schaubl <lionel.schaub@bluewin.ch>
2014-04-03 15:33:25 +0200

    Fixes for the script which create an anonymized backup of the database.

--- backup/src/slipstream-backup-db-anonymized.sh
+++ backup/src/slipstream-backup-db-anonymized.sh
@@ -2,20 +2,20 @@
 
 DB_PATH='/opt/slipstream/SlipStreamDB/'
 CONFIG='/etc/slipstream/slipstream-backup.conf'
+SQL_ANONYMIZE_FILE='/opt/slipstream/backup/anonymize.sql'
 
 HSQLDB_PATH='/opt/hsqldb/lib/hsqldb.jar'
 SQLTOOL_PATH='/opt/hsqldb/lib/sqltool.jar'
 
-SQL_ANONYMIZE_FILE='anonymize.sql'
-
 source $CONFIG
 
 AMAZON_BUCKET=${AMAZON_BUCKET_ANONYMIZED:?"Define Amazon S3 anonymize bucket in $CONFIG."}
-SS_HOSTNAME=${SS_HOSTNAME:?"Define the host IP/hostname in $CONFIG.
+SS_HOSTNAME=${SS_HOSTNAME:?"Define the host IP/hostname in $CONFIG."}
 
+LANG=en_US.utf8
 TIMESTAMP=`date --utc "+%Y-%m-%dT%H%MZ"`
 CUR_DIR=`pwd`
-BUNDLE_NAME="/tmp/slipstream.backup.anonymized.${SS_HOSTNAME}.${TIMESTAMP}"
+BUNDLE_NAME="slipstream.backup.anonymized.${SS_HOSTNAME}.${TIMESTAMP}"
 BUNDLE=/tmp/$BUNDLE_NAME
 
 mkdir $BUNDLE
@@ -27,18 +27,22 @@ DB_PID=$!
 
 java -cp $HSQLDB_PATH -jar $SQLTOOL_PATH --inlineRc=url=jdbc:hsqldb:hsql://localhost:9999/slipstream_ano,user=sa,password= --autoCommit $SQL_ANONYMIZE_FILE || echo 'Error during anonymization'
 
-kill -9 $DB_PID
+kill $DB_PID
+sleep 5
+kill -9 $DB_PID 2>/dev/null || true
+
+tar czf ${BUNDLE}.tgz ${BUNDLE}/*
 
 BACKUP="${AMAZON_BUCKET}/${BUNDLE_NAME}"
 
-output=$(/opt/slipstream/backup/s3curl.pl --id sixsq --put $BUNDLE -- $BACKUP 2>&1)
+output=$(/opt/slipstream/backup/s3curl.pl --id sixsq --put $BUNDLE.tgz -- $BACKUP 2>&1)
 [ "$?" -eq "0" ] && \
     echo "SlipStream Anonymized Backup Successful. $BACKUP" || \
     echo "FAILURE $BACKUP: $output"
 
 cd $CUR_DIR
 
-rm -Rf $BUNDLE
+rm -Rf $BUNDLE $BUNDLE.tgz
 
 exit 0
 

d86ab301520c920f48db987211c139f23d62c4df
Stephane Tavera <stephane.tavera@gmail.com>
2016-05-19 16:07:34 +0200

    Initial version of riemann streams for nuvlabox

---
+++ riemann/jar/project.clj
@@ -0,0 +1,11 @@
+(defproject nuvlabox-riemann "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies
+  [[org.clojure/clojure "1.8.0"]
+   [clj-http "2.0.0"]
+   [riemann "0.2.11"]
+   [environ "1.0.1"]
+   [org.clojure/data.json "0.2.6"]])

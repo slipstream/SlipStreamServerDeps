197e85cb1db201ff37e778fc07b9ed9549ee573c
Stephane Tavera <stephane.tavera@gmail.com>
2016-05-20 18:23:36 +0200

    Update last-online independently of state change

--- riemann/streams/nuvlabox-streams.clj
+++ riemann/streams/nuvlabox-streams.clj
@@ -1,11 +1,11 @@
 (require '[sixsq.slipstream.api.service-offer :as service-offer])
 
-;;(logging/init {:file "/var/log/riemann/riemann.log"})
-;;
-;;(let [host "0.0.0.0"]
-;;     (tcp-server {:host host})
-;;     (udp-server {:host host})
-;;     (ws-server  {:host host}))
+;; (logging/init {:file "/var/log/riemann/riemann.log"})
+;; 
+;; (let [host "0.0.0.0"]
+;;      (tcp-server {:host host})
+;;      (udp-server {:host host})
+;;      (ws-server  {:host host}))
 
 (def expiration-watch       1)
 (def default-ttl            5)
@@ -25,27 +25,30 @@
 
 (def states (atom {}))
 
-(defn- name-id-state
+(defn- name-id-state-last-online
        [connector]
-       [(get-in connector [:connector :href])
-        (:id connector)
-        (if (= :ok (-> connector :state keyword)) :ok :nok)])
+       {:name         (keyword (get-in connector [:connector :href]))
+        :id           (:id connector)
+        :state        (if (= :ok (-> connector :state keyword)) :ok :nok)
+        :last-online  (:last-online connector)})
 
 (defn- record-connector
-       [m [name id state]]
-       (assoc m (keyword name) {:id id :state state}))
+       [m name-id-state-last-online]
+       (assoc
+         m
+         (:name name-id-state-last-online)
+         (dissoc name-id-state-last-online :name)))
 
 (defn- fetch-topology
        []
        (info "Fetching map states")
        (->>  (try-get-connectors)
-             (map name-id-state)
+             (map name-id-state-last-online)
              (reduce record-connector {})))
 
 (defn- all-states-nok
        [map-states]
-       (into {} (map (fn[[k v]]
-                        [k (assoc v :state :nok)]) map-states)))
+       (into {} (map (fn[[k v]] [k (assoc v :state :nok)]) map-states)))
 
 (defn- build-topology!
        [& [{:keys [force-nok]}]]
@@ -55,44 +58,53 @@
             (info "Map states " (if force-nok "force all to nok" "") ":" @states)
             @states))
 
-(defn timestamp-if-online
-  [state-update state]
-  (if (= :ok state)
-    (assoc state-update :last-online (str (java.util.Date.)))
-    state-update))
+(defn- update-state!
+  [name state]
+  (swap! states #(assoc-in % [name :state] state))
+  (info "State changed for " name ", new state: " state))
 
-(defn- update-state
-       [name id state]
-       (io (service-offer/update-connector
-             (->  {:id id :state state}
-                  (timestamp-if-online state))))
-       (swap! states #(assoc-in % [name :state] state))
-       (info "State changed for " name "id: " id ", new state: " state))
+(defn- update-last-online!
+  [name state]
+  (when (= :ok state)
+    (let [new-last-online (str (java.util.Date.))]
+      (swap! states #(assoc-in % [name :last-online] new-last-online))
+      (info "Last online changed for " name " with: " new-last-online))))
+
+(defn- send-state
+       [name id-state-last-online]
+       (io (service-offer/update-connector id-state-last-online))
+       (info "State sent for " name ": " id-state-last-online))
+
+(defn- update-send-state!
+       [name id state last-online]
+       (update-state! name state)
+       (update-last-online! name state)
+       (send-state name (name @states)))
 
 (defn- receive-state
        [event state]
        (let [name           (-> event :nuvlabox-name keyword)
              states         @states
              states         (if-not (contains? states name) (build-topology!) states)
-             current-state  (get-in states [name :state])
-             id             (get-in states [name :id])]
-
+               current-state  (get-in states [name :state])
+             id             (get-in states [name :id])
+             last-online    (get-in states [name :last-online])]
             (if-not id
                     (warn "Unknown name : " name)
                     (if-not (= state current-state)
-                            (update-state name id state)
-                            (info "No change for " name ", state: " state)))))
+                      (update-send-state! name id state last-online)
+                      (update-last-online! name state)))))
 
-(defn- update-all
+(defn- send-all-states
        []
-       (doseq [[name {:keys [id state]}] @states]
-              (update-state name id state)))
+       (doseq [[name id-state-last-online] @states]
+              (send-state name id-state-last-online)))
 
 (let [index (default :ttl default-ttl (index))]
 
      (build-topology! {:force-nok true})
 
-     (update-all)
+     (send-all-states)
 
      (streams
        index

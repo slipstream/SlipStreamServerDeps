82c02520e38f2406e05eb263050d9efda2f12019
Stephane Tavera <stephane.tavera@gmail.com>
2016-09-22 17:42:57 +0200

    lowercase snapshot name

--- backup/src/es/slipstream-es-backup.sh
+++ backup/src/es/slipstream-es-backup.sh
@@ -4,17 +4,8 @@ export LANG=en_US.utf8
 
 set -e
 
-CONFIG=/etc/slipstream/slipstream-es-backup.conf
-source $CONFIG
-
-# Close indices
-# TODO Check if this is actually needed
-curl -XPOST 'localhost:9200/resources-index/_close'
-
-BACKUP_NAME=es.snapshot.${SS_HOSTNAME}.$(date --utc "+%Y-%m-%dT%H%MZ")
+# Note that snapshotname must be lowercase.
+BACKUP_NAME=es.snapshot.$HOSTNAME.$(date --utc "+%Y-%m-%dt%H%Mz")
 
 curl -XPUT http://localhost:9200/_snapshot/es_backup/$BACKUP_NAME?wait_for_completion=true
 
-# Reopen indices
-# TODO Check if this is actually needed
-curl -XPOST 'localhost:9200/resources-index/_open'
--- backup/src/es/slipstream-es-restore-from-backup.sh
+++ backup/src/es/slipstream-es-restore-from-backup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -x
 
-SNAPSHOT_DATE=${1:?"Snapshot date is required as an argument.e.g 2016-09-19T1032Z"}
+SNAPSHOT_DATE=${1:?"Snapshot date is required as an argument. List all snapshots with: curl 'localhost:9200/_cat/snapshots/es_backup?v'
+"}
 
 set -e
 
 # Close indices before restoring
 curl -XPOST localhost:9200/resources-index/_close
 
-curl -XPOST http://localhost:9200/_snapshot/es_backup/snapshot_$SNAPSHOT_DATE/_restore
+curl -XPOST http://localhost:9200/_snapshot/es_backup/es.snapshot.$SNAPSHOT_DATE/_restore
 
 # Reopen indices after restoring
 curl -XPOST http://localhost:9200/resources-index/_open

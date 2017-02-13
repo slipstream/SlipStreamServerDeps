20832c81f18d230e39e303afef98cd95d05af85f
Konstantin Skaburskas <Konstantin.Skaburskas@gmail.com>
2017-02-13 17:01:11 +0100

    fix: allow to reopen indices when restore failed.

--- backup/src/es/slipstream-es-restore-from-backup.sh
+++ backup/src/es/slipstream-es-restore-from-backup.sh
@@ -6,11 +6,16 @@ SNAPSHOT_DATE=${1:?"Snapshot date is required as an argument. List all snapshots
 set -e
 
 # Close indices before restoring
-curl -fv -XPOST localhost:9200/resources-index/_close
+curl -XPOST localhost:9200/resources-index/_close
 
-curl -fv -XPOST http://localhost:9200/_snapshot/es_backup/es.snapshot.$SNAPSHOT_DATE/_restore
+set +e
+curl -f -XPOST http://localhost:9200/_snapshot/es_backup/es.snapshot.$SNAPSHOT_DATE/_restore
+rc=$?
+if [[ $rc -eq 0 ]]; then result=successful; else result=failed; fi
+set -e
 
 # Reopen indices after restoring
-curl -fv -XPOST http://localhost:9200/resources-index/_open
+curl -XPOST http://localhost:9200/resources-index/_open
 
-echo "Restore done"
+echo "Restore $result."
+exit $rc

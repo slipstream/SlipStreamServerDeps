4ddba113284839258562de167c9ea4a480bcbea9
Charles Loomis <cal@sixsq.com>
2018-01-10 08:11:18 +0100

    update nagios configuration

--- nagios/nagios-install.sh
+++ nagios/nagios-install.sh
@@ -1,15 +1,41 @@
 #!/bin/sh
 
-yum install -y ntp
-service ntpd start
+yum install -y epel-release
 
-yum install -y nagios nagios-plugins-all nagios-plugins-nrpe httpd php
+yum clean all
+
+yum install -y git wget ntp nagios nagios-plugins-all nagios-plugins-nrpe httpd php
+
+# disable SELinux
+setenforce 0
+sed s/SELINUX=enforcing/SELINUX=permissive/g -i /etc/selinux/config
+
+# enable services
+systemctl enable nagios
+systemctl enable httpd
+systemctl enable ntpd
+
+# install sixsq monitoring targets
+cd /root
+git clone https://github.com/slipstream/SlipStreamServerDeps.git
+command cp -R --remove-destination /root/SlipStreamServerDeps/nagios/etc/nagios/* /etc/nagios/
+
+# fix ownership and permissions
+chown -R root:nagios /etc/nagios
+chgrp apache /etc/nagios/passwd
+usermod -G apache,nagios apache
+
+# start services
+systemctl start ntpd
+systemctl start nagios
+systemctl start httpd
 
 # Install Slack notifications
-yum install perl-libwww-perl perl-Net-SSLeay perl-Crypt-SSLeay
+yum install -y perl-libwww-perl perl-Net-SSLeay perl-Crypt-SSLeay
 wget --no-check-certificate -O /usr/local/bin/slack_nagios.pl \
      https://raw.github.com/tinyspeck/services-examples/master/nagios.pl
 chmod 755 /usr/local/bin/slack_nagios.pl
+
 echo 'MANUAL STEP IS REQUIRED
 # Edit /usr/local/bin/slack_nagios.pl according to 
 # https://sixsq.slack.com/services/B026T5CM7

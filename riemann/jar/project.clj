37211cba3792de4f80170f08cb81beb5691f2257
Charles Loomis <cal@sixsq.com>
2018-05-06 10:57:13 +0200

    update parent

--- riemann/jar/project.clj
+++ riemann/jar/project.clj
@@ -13,8 +13,9 @@
   :plugins [[lein-parent "0.3.2"]
             [lein-localrepo "0.5.4"]]
 
-  :parent-project {:coords  [sixsq/slipstream-parent "5.3.2"]
-                   :inherit [:min-lein-version
+  :parent-project {:coords  [sixsq/slipstream-parent "5.3.3"]
+                   :inherit [:plugins
+                             :min-lein-version
                              :managed-dependencies
                              :repositories
                              :deploy-repositories]}

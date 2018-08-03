99985c98919472c21ea0c7b5f818d43580384f49
sixsq-hudson <hudson@sixsq.com>
2018-08-03 09:16:52 +0000

    next development version

--- riemann/jar/project.clj
+++ riemann/jar/project.clj
@@ -1,6 +1,6 @@
-(def +version+ "3.56")
+(def +version+ "3.57-SNAPSHOT")
 
-(defproject com.sixsq.slipstream/SlipStreamRiemann-jar "3.56"
+(defproject com.sixsq.slipstream/SlipStreamRiemann-jar "3.57-SNAPSHOT"
 
   :description "Riemann Server for Autoscaling"
 
